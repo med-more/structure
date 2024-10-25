95a6c959720bffc292eb4da978016e55d7e7175c
mohammed <166568621+med-more@users.noreply.github.com>
2024-10-25 12:08:18 +0200

    last changes : add function

--- problem.c
+++ problem.c
@@ -1,56 +1,137 @@
 #include<stdio.h>
-#include<string.h>
+#define max 50
 
- struct personne
+
+
+// déclarer structure et embraqué
+typedef struct 
+{
+    char rue[100];
+    char ville[100];
+    char code_postal[10];
+}adress;
+
+typedef struct 
 {
     char nom[50];
     int age;
-    struct Ad adress; 
-};
+    adress add;
+}personne;
 
- struct Ad
-{
-    char rue[50];
-    char ville[60];
-    char code_postale[50];
-};
+//la fonction qui permet de creat nouvelle personne 
 
+personne add(){
+
+    personne p;
+
+    printf("entrer le nom :");
+    scanf("%s", &p.nom);
+
+    printf("entrer l'age :");
+    scanf("%s", &p.age);
+
+    printf("entrer la rue :");
+    scanf("%s", &p.add.rue);
+
+    printf("entrer la ville :");
+    scanf("%s", &p.add.ville);
+
+    printf("entrer le code postale :");
+    scanf("%s", &p.add.code_postal);
+
+    return p;
+}
+// fonction permet de afficher personne 
+void afficher(personne p){
+
+    printf("\nNom : %s\n", p.nom);
+    printf("\nAge : %d\n", p.age);
+    printf("\nAdress de personne\n");
+    printf("\nRue : %s\n", p.add.rue);
+    printf("\nVille : %s\n", p.add.ville);
+    printf("\ncode postale : %s\n", p.add.code_postal);   
+}
+
+
+//fonction pour modifier 
+
+void edit(personne p[],int index){
+
+    printf("modifier les information de :%s\n", p[index].nom);
+
+        printf("entrer le nouveu nom :");
+        scanf("%s", &p[index].nom);
+
+        printf("entrer le nouveu age :");
+        scanf("%d", &p[index].age);
+
+        printf("entrer le nouveu rue :");
+        scanf("%s", &p[index].add.rue);
+
+        printf("entrer la nouvelle ville :");
+        scanf("%s", &p[index].add.ville);
+
+        printf("entrer le nouveu code postale :");
+        scanf("%s", &p[index].add.code_postal);
+}
+
+
+//principal
 int main(){
 
+    int choix;
+    personne personnes[max];
+    int nbr_personne=0;
 
-for (int i = 0; i < 5; i++)
-{
- struct personne p1[i];
- printf("entrer le nom :\n");
-scanf("%s", &p1[i].nom);
+    for (;;)
+    {
+        printf("\nles choix :\n");
+        printf("\n 1.creer personne\n");
+        printf("\n 2.Afficher les personnes\n");
+        printf("\n 3.Modifier  personne\n");
+        printf("\n 4.supprimer  personne\n");
+        printf("\n 5.Quitter\n");
+        printf("choissir un choix :");
+        scanf("%d", &choix); 
 
-printf("entrer le l'age :\n");
-scanf("%s", &p1[i].age);
+        switch (choix)
+        {
+        case 1:
+            personnes [nbr_personne]=add();
+            nbr_personne++;
+            printf("le personne est bien ajouter");
+            break;
+        case 2:
+            
+          for (int i = 0; i < nbr_personne; i++)
+          {
+           
+            printf("\nNom : %s\n", personnes[i].nom);
+            printf("\nAge : %d\n", personnes[i].age);
+            printf("\nAdress de personne\n");
+            printf("\nRue : %s\n", personnes[i].add.rue);
+            printf("\nVille : %s\n", personnes[i].add.ville);
+            printf("\ncode postale : %s\n", personnes[i].add.code_postal);
+          }
+          break;
+          case 3:
 
-printf("entrer le rue :\n");
-scanf("%s", &p1[i].adress.rue);
+          int index;
+          printf("entrer l'index pour modifier :");
+          scanf("%d", &index);
+          if (index>=0 && index<nbr_personne)
+          {
+            edit(personnes,index);
+          }
+          else
+          {
+            printf("pas de personne a modifier");
+          }
+        break;
 
-printf("entrer la ville :\n");
-scanf("%s", &p1[i].adress.ville);
-
-printf("entrer le code postale :\n");
-scanf("%s", &p1[i].adress.code_postale); 
+        default:
+            break;
+        }
+    }
+    return 0;
 }
-
-printf("------------------les information----------------");
-
-for (int i = 0; i < 5; i++)
-{
- struct personne p1[i];
-
- printf("Nom: %s\n", p1[i].nom);
- printf("age: %s\n", p1[i].age);
- printf("adress: %s\n", p1[i].adress.rue);
- printf("ville: %s\n", p1[i].adress.ville);
- printf("code postale: %s\n", p1[i].adress.code_postale);
-}
-
-
-
-}
-
